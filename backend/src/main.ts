94189cc4edacf14d303a113cbf36a76344367253
CaoMinhTri972002 <144448561+CaoMinhTri972002@users.noreply.github.com>
2025-09-14 16:32:23 +0700

    tri fix backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); // ← thêm dòng này để cho phép frontend khác port fetch
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
